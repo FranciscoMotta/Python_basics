86535820d26099e4ba430f5ed7cb300737b85d3b
Francisco Motta <jmottaclaudio@gmail.com>
2022-01-02 18:46:10 -0500

    ultimo básico

---
+++ estructuras_datos.py
@@ -0,0 +1,10 @@
+def run():
+    mi_dicc = {
+        'llave1': 1,
+        'llave2': 2,
+        'llave3': 3,
+    }
+    print(mi_dicc['llave1'])
+
+if __name__ == '__main__':
+    run()
---
+++ password_gen.py
@@ -0,0 +1,31 @@
+import random
+
+def generar_cont():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    caracte = MAYUS + MINUS + NUMS + CHARS
+
+    contrase = []
+
+    for i in range(15):
+        caracter_ran = random.choice(caracte) # Elegimos un elementos aleatorio
+        contrase.append(caracter_ran) # agregamos a la lista de la contraseña
+    
+    # convertimos una lista a string
+    contrase = ''.join(contrase)
+
+    return contrase
+
+
+
+def main():
+    passw = generar_cont()
+    print('Tu nueva clave es: ' + passw)
+
+
+
+if __name__ == '__main__':
+    main()
